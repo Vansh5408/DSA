8fa9621b58c6eaddb8f3ce31f2e749c6dcfea024
Vansh jindal <vanshjindal1450@gmail.com>
2025-12-21 15:03:09 +0530

    recursion

---
+++ reverse_Recursion.java
@@ -0,0 +1,15 @@
+package lecture26;
+
+public class reverse_Recursion {
+	public static void main(String[]args) {
+		int n =5;
+		rev(n);
+	}
+	public static void rev(int n) {
+		if(n==0) {
+			return ;
+		}
+		System.out.println(n);
+		rev(n-1);
+	}
+}
